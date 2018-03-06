173d98ddf2266ad69d5c166ba0d81ffb72b868ec
Quang Nguyen <ngocquang19877@gmail.com>
2018-03-06 18:13:53 +0700

    Implement charting

--- source/js/views/Home.jsx
+++ source/js/views/Home.jsx
@@ -3,10 +3,13 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { increment } from 'actions/app';
 import ReactGridLayout from 'react-grid-layout';
+import {Line, Bar, Pie} from 'react-chartjs';
+import ChartData from '../../test/data.js';
 
 @connect(state => ({
   counter: state.app.get('counter'),
 }))
+
 export default class Home extends Component {
   static propTypes = {
     // from react-redux connect
@@ -15,22 +18,36 @@ export default class Home extends Component {
 
   handleTestButtonClick = () => {
     const { dispatch } = this.props;
-
     dispatch(increment());
   }
 
   render() {
-    var layout = [
-      {i: 'a', x: 0, y: 0, w: 1, h: 2, static: true},
-      {i: 'b', x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4},
-      {i: 'c', x: 4, y: 0, w: 1, h: 2}
+
+    let layout = [
+      {i: 'a', x: 0, y: 0, w: 1, h: 2},
+      {i: 'b', x: 1, y: 0, w: 3, h: 2},
+      {i: 'c', x: 2, y: 0, w: 1, h: 2}
     ];
     
+    let chartOptions={
+      series: { pie: { show: true } }, 
+      grid  : { hoverable: true }, 
+      tooltip: true,
+      tooltipOpts: { content: "%p.0%, %s", shifts: {x: 20,y: 0},                   
+      defaultTheme: false }
+    };
+
     return (
       <ReactGridLayout className="layout" layout={layout} cols={12} rowHeight={30} width={1200}>
-        <div key="a">a</div>
-        <div key="b">b</div>
-        <div key="c">c</div>
+        <div className="carbox" key="a">
+          <Line data={ChartData.LineData} options={ChartData.LineDataOptions}/>
+        </div>
+        <div className="carbox" key="b">
+          <Bar data={ChartData.BarData} options={ChartData.BarDataOptions}/>
+        </div>
+        <div className="carbox" key="c">
+          <Pie data={ChartData.PieData} options={ChartData.PieDataOptions}/>
+        </div>
       </ReactGridLayout>
     );
   }
---
+++ source/test/data.js
@@ -0,0 +1,187 @@
+const ChartData = {
+    LineData: {
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [
+            {
+                label: "My First dataset",
+                fillColor: "rgba(220,220,220,0.2)",
+                strokeColor: "rgba(220,220,220,1)",
+                pointColor: "rgba(220,220,220,1)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(220,220,220,1)",
+                data: [65, 59, 80, 81, 56, 55, 40]
+            },
+            {
+                label: "My Second dataset",
+                fillColor: "rgba(151,187,205,0.2)",
+                strokeColor: "rgba(151,187,205,1)",
+                pointColor: "rgba(151,187,205,1)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(151,187,205,1)",
+                data: [28, 48, 40, 19, 86, 27, 90]
+            }
+        ]
+    },
+
+    LineDataOptions: {
+
+        ///Boolean - Whether grid lines are shown across the chart
+        scaleShowGridLines : true,
+
+        //String - Colour of the grid lines
+        scaleGridLineColor : "rgba(0,0,0,.05)",
+
+        //Number - Width of the grid lines
+        scaleGridLineWidth : 1,
+
+        //Boolean - Whether to show horizontal lines (except X axis)
+        scaleShowHorizontalLines: true,
+
+        //Boolean - Whether to show vertical lines (except Y axis)
+        scaleShowVerticalLines: true,
+
+        //Boolean - Whether the line is curved between points
+        bezierCurve : true,
+
+        //Number - Tension of the bezier curve between points
+        bezierCurveTension : 0.4,
+
+        //Boolean - Whether to show a dot for each point
+        pointDot : true,
+
+        //Number - Radius of each point dot in pixels
+        pointDotRadius : 4,
+
+        //Number - Pixel width of point dot stroke
+        pointDotStrokeWidth : 1,
+
+        //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
+        pointHitDetectionRadius : 20,
+
+        //Boolean - Whether to show a stroke for datasets
+        datasetStroke : true,
+
+        //Number - Pixel width of dataset stroke
+        datasetStrokeWidth : 2,
+
+        //Boolean - Whether to fill the dataset with a colour
+        datasetFill : true,
+        
+        //String - A legend template
+        legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"><%if(datasets[i].label){%><%=datasets[i].label%><%}%></span></li><%}%></ul>",
+        
+        //Boolean - Whether to horizontally center the label and point dot inside the grid
+        offsetGridLines : false
+    },
+
+    BarData: {
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [
+            {
+                label: "My First dataset",
+                fillColor: "rgba(220,220,220,0.5)",
+                strokeColor: "rgba(220,220,220,0.8)",
+                highlightFill: "rgba(220,220,220,0.75)",
+                highlightStroke: "rgba(220,220,220,1)",
+                data: [65, 59, 80, 81, 56, 55, 40]
+            },
+            {
+                label: "My Second dataset",
+                fillColor: "rgba(151,187,205,0.5)",
+                strokeColor: "rgba(151,187,205,0.8)",
+                highlightFill: "rgba(151,187,205,0.75)",
+                highlightStroke: "rgba(151,187,205,1)",
+                data: [28, 48, 40, 19, 86, 27, 90]
+            }
+        ]
+    },
+
+    BarDataOptions: {
+        //Boolean - Whether the scale should start at zero, or an order of magnitude down from the lowest value
+        scaleBeginAtZero : true,
+    
+        //Boolean - Whether grid lines are shown across the chart
+        scaleShowGridLines : true,
+    
+        //String - Colour of the grid lines
+        scaleGridLineColor : "rgba(0,0,0,.05)",
+    
+        //Number - Width of the grid lines
+        scaleGridLineWidth : 1,
+    
+        //Boolean - Whether to show horizontal lines (except X axis)
+        scaleShowHorizontalLines: true,
+    
+        //Boolean - Whether to show vertical lines (except Y axis)
+        scaleShowVerticalLines: true,
+    
+        //Boolean - If there is a stroke on each bar
+        barShowStroke : true,
+    
+        //Number - Pixel width of the bar stroke
+        barStrokeWidth : 2,
+    
+        //Number - Spacing between each of the X value sets
+        barValueSpacing : 5,
+    
+        //Number - Spacing between data sets within X values
+        barDatasetSpacing : 1,
+
+        //String - A legend template
+        legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].fillColor%>\"><%if(datasets[i].label){%><%=datasets[i].label%><%}%></span></li><%}%></ul>"
+    },
+
+    PieData: [
+        {
+            value: 300,
+            color:"#F7464A",
+            highlight: "#FF5A5E",
+            label: "Red"
+        },
+        {
+            value: 50,
+            color: "#46BFBD",
+            highlight: "#5AD3D1",
+            label: "Green"
+        },
+        {
+            value: 100,
+            color: "#FDB45C",
+            highlight: "#FFC870",
+            label: "Yellow"
+        }
+    ],
+
+    PieDataOptions: {
+        //Boolean - Whether we should show a stroke on each segment
+        segmentShowStroke : true,
+    
+        //String - The colour of each segment stroke
+        segmentStrokeColor : "#fff",
+    
+        //Number - The width of each segment stroke
+        segmentStrokeWidth : 2,
+    
+        //Number - The percentage of the chart that we cut out of the middle
+        percentageInnerCutout : 50, // This is 0 for Pie charts
+    
+        //Number - Amount of animation steps
+        animationSteps : 100,
+    
+        //String - Animation easing effect
+        animationEasing : "easeOutBounce",
+    
+        //Boolean - Whether we animate the rotation of the Doughnut
+        animateRotate : true,
+    
+        //Boolean - Whether we animate scaling the Doughnut from the centre
+        animateScale : false,
+
+        //String - A legend template
+        legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].fillColor%>\"><%if(segments[i].label){%><%=segments[i].label%><%}%></span></li><%}%></ul>"
+    }
+}
+
+  export default ChartData;
